1dcc46453a6bad2c075d32c320951b06c52ba2f3
seiya_t <kenose0328@gmail.com>
2021-07-09 05:54:26 +0900

    [add]初期stateの設定

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import defaultDataset from "./dataset";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: "init",
+      dataset: defaultDataset,
+      open: false,
+    };
+  }
   render() {
     return (
       <div>
-      </div>
+
+      </div>;
     );
   }
 }
