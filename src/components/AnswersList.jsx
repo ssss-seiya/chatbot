e77cbd6fe05c3250aa5a05ee5af1825bb200ac68
seiya_t <kenose0328@gmail.com>
2021-07-10 05:50:07 +0900

    [add]引数を渡すように

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Answer} from './index'
 
-const AnswersList = () => {
+const AnswersList = (props) => {
   return (
     <div className='c-grid__answer'>
-      <Answer />
-      <Answer />
-      <Answer />
-      <Answer />
+      <Answer content={'hoge'}/>
+      <Answer content={'fuga'}/>
+      <Answer content={'foo'}/>
+      <Answer content={'bar'}/>
     </div>
   )
 }
