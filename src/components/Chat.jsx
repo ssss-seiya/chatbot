987be3bfa0bf9a56b766381525e9c9d017e08fc4
seiya_t <kenose0328@gmail.com>
2021-07-10 07:38:44 +0900

    [add]Chatsからのpropsを受け取るように

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
 
-const Chat = () => {
+const Chat = (props) => {
+  const isQuestion = (props.type === 'question');
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
+
   return (
-    <ListItem>
+    <ListItem className={classes}>
       <ListItemAvatar>
         <Avatar alt="icon" src="static/images/avatar/1.jpg" />
       </ListItemAvatar>
-      <div className='p-chat__bubble'>ダミーテキスト</div>
+      <div className='p-chat__bubble'>{props.text}</div>
     </ListItem>
   )
 }
