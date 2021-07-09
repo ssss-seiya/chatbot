ab02d3bd419e517f132e54b9ffa70dea64e5c543
seiya_t <kenose0328@gmail.com>
2021-07-10 07:39:14 +0900

    [add]Chatにpropsを渡すように

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -17,10 +17,9 @@ const Chats = (props) => {
 
   return (
     <List className={classes.root}>
-      <Chat />
-      <Chat />
-      <Chat />
-      <Chat />
+      {props.chats.map((chat, index) => {
+        return <Chat text={chat.text} type={chat.type} key={index.toString()}/>
+      })}
     </List>
   )
 }
