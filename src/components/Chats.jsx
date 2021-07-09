bf18aa021472ac1c361123d28790733deb8a20fd
seiya_t <kenose0328@gmail.com>
2021-07-10 07:04:56 +0900

    [create]Chatsコンポーネントの雛形

---
+++ src/components/Chats.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import { Chat } from './index';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '36ch',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const Chats = (props) => {
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>
+      <Chat />
+      <Chat />
+      <Chat />
+      <Chat />
+    </List>
+  )
+}
+
+export default Chats
