b11d7d1a54cef27df5243ba814d095c1b595e692
seiya_t <kenose0328@gmail.com>
2021-07-11 15:18:48 +0900

    [add]stateの説明コメント

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      answers: [],
-      chats: [],
-      currentId: "init",
-      dataset: defaultDataset,
-      open: false,
+      answers: [], // 回答コンポーネントに表示するデータ
+      chats: [], // チャットコンポーネントに表示するデータ
+      currentId: "init", // 現在の質問ID
+      dataset: defaultDataset, // 質問と回答のデータセット
+      open: false, // 問い合わせフォーム用モーダルの開閉を管理
     }
     this.selectAnswer = this.selectAnswer.bind(this)
   }
